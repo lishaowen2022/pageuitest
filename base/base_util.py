480b9869f40afec21bb6b692682ee380746d345a
KFC <109707736+lishaowen2022@users.noreply.github.com>
2022-12-05 22:41:28 +0800

    Update base_util.py

--- base/base_util.py
+++ base/base_util.py
@@ -27,10 +27,11 @@ class BaseUtil:
         
         if system_driver.lower() == "linux":
             chrome_options = Options()
-            chrome_options.add_argument('no-sandbox')
-            chrome_options.add_argument('headless')
-            chrome_options.add_argument('disable-dev-shm-usage')
-            chrome_options.add_argument('disable-gpu')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome(chrome_options=chrome_options)
             return driver
 
 
